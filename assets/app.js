caf4a5357b955342db90d514816310b44ace6f8c
nbalder <nbalder@gmail.com>
2018-07-21 13:04:42 -0500

    testing a commit

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$("#library").on("click", displayMap)
+    $("#library").on("click", displayMap)
 
-function displayMap(){
-<<<<<<< HEAD
-    $('<iframe src="https://embed.waze.com/en/iframe?zoom=12&lat=30.2672&lon=-97.7430&pin=1" width="300" height="400"></iframe>').appendTo(".content");
-    $('iframe').contents().find(".content").html();
-}
+    function displayMap() {
+        $('<iframe src="https://embed.waze.com/en/iframe?zoom=12&lat=30.2672&lon=-97.7430&pin=1" width="300" height="400"></iframe>').appendTo(".content");
+        $('iframe').contents().find(".content").html();
+    }
 })
-      
-=======
-    $("#button-content").clear();
-    var mlink = <iframe src="https://embed.waze.com/en/iframe?zoom=12&lat=30.2672&lon=-97.7430" width="300" height="400"></iframe>
-    mlink.appendTo("#content");
-return false;
-}
-})
->>>>>>> 558019ddb4699d9fb3e5c1c2cabf51b1d3254150
