154295878f182db5d2fb844782d3db3227cabed7
nbalder <nbalder@gmail.com>
2018-07-23 23:50:29 -0500

    multiple markers working on map now

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,155 @@
 $(document).ready(function () {
+    $("#library").on("click", initMap)
+    $("#restaurant").on("click", initMap)
+    $("#coffee-shop").on("click", initMap)
+    var libraryMarker = [{
+            coords: {
+                lat: 30.2659,
+                lng: -97.8218
+            },
+            content: 'Austin Public Library'
+        },
+        {
+            coords: {
+                lat: 30.4424,
+                lng: -97.6292
+            },
+            content: 'Pfluggerville Library'
+        },
+        {
+            coords: {
+                lat: 30.4337,
+                lng: -97.7731
+            },
+            content: 'Spicewood Springs Branch Library'
+        },
+        {
+            coords: {
+                lat: 30.3233,
+                lng: -97.8108
+            },
+            content: 'Yarborough Branch Library'
+        },
+        {
+            coords: {
+                lat: 30.1922,
+                lng: -97.8472
+            },
+            content: 'Pleasant Hill Branch Library'
+        }
+    ];
+    var RestaurantMarker = [{
+            coords: {
+                lat: 30.3951,
+                lng: -97.8475
+            },
+            content: 'Black Walnut Cafe'
+        },
+        {
+            coords: {
+                lat: 30.2605,
+                lng: -97.7337
+            },
+            content: 'Cenote'
+        },
+        {
+            coords: {
+                lat: 30.3323,
+                lng: -97.7351
+            },
+            content: 'Brentwood Social House'
+        },
+        {
+            coords: {
+                lat: 30.2955,
+                lng: -97.7418
+            },
+            content: 'Spider House'
+        },
+        {
+            coords: {
+                lat: 30.3922,
+                lng: -97.7324
+            },
+            content: 'BJs Restaurant & Brewhouse'
+        }
+    ];
+    var coffeeShopMarker = [{
+            coords: {
+                lat: 30.2954,
+                lng: -97.7842
+            },
+            content: 'Mozarts Coffee Roaster'
+        },
+        {
+            coords: {
+                lat: 30.2935,
+                lng: -97.9760
+            },
+            content: 'Cherrywood Coffeehouse'
+        },
+        {
+            coords: {
+                lat: 30.1373,
+                lng: -97.7966
+            },
+            content: 'Opera Cafe & Coffee House'
+        },
+        {
+            coords: {
+                lat: 30.2669,
+                lng: -97.7200
+            },
+            content: 'Figure 8 Coffee Purveyors'
+        },
+        {
+            coords: {
+                lat: 30.4780,
+                lng: -97.7663
+            },
+            content: 'Cuppa Austin'
+        }
+    ];
 
-    $("#library").on("click", displayMap)
+    var map;
 
-    function displayMap() {
-        $('<iframe src="https://embed.waze.com/en/iframe?zoom=12&lat=30.2672&lon=-97.7430&pin=1" width="300" height="400"></iframe>').appendTo(".content");
-        $('iframe').contents().find(".content").html();
+    function initMap() {
+        var options = {
+            zoom: 10,
+            center: {
+                lat: 30.2867,
+                lng: -97.7297
+            }
+        }
+        var map = new google.maps.Map(document.getElementById('map'), options);
+
+
+        for (var i = 0; i < coffeeShopMarker.length; i++) {
+            addMarker(coffeeShopMarker[i]);
+        }
+
+        function addMarker(props) {
+            var marker = new google.maps.Marker({
+                position: props.coords,
+                map: map,
+            });
+
+            if (props.content) {
+                var infoWindow = new google.maps.InfoWindow({
+                    content: props.content
+                });
+
+                marker.addListener('mouseover', function () {
+                    infoWindow.open(map, marker);
+                });
+                // NOT WORKING!!
+                // marker.addListener('mouseleave',function(){
+                // infoWindow.close(map,marker);
+                // });
+
+            }
+        }
     }
+
+
 })
